b6f1f25a76910caab2c58bc7a0963c707420e71e
Eftekher Husain <32465908+ehusain000@users.noreply.github.com>
2020-10-21 15:38:28 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+// # used for including a library
+// include means include this library
+// stdio C Standard Input and Output Library
+
+
+
+// int returns an integer
+//main function is needed to compile code
+// a function is nothing a bunch of instruction
+// function is the actual body of the program
+int
+main ()
+{
+  int a = 20;
+  int b = 15.5;
+  double sum = a + b;
+  printf ("sum is: %f ", sum);
+
+  return 0;
+}
